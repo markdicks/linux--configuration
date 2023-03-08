7beade81f9ffda7fb8f9c9b6971c13aadd770e08
Mark Dicks <117034609+markdicks@users.noreply.github.com>
2023-03-08 02:28:25 +0200

    Updated 4 main things, check detailed comment
    Fixed the background running process, closing the terminal was closing the ide. 
    Made the folder itself invisible in the home directory from wherever it was run to prevent issues with accidental deletions
    Fixed the error with command not been found due to alias not really updating
    Fixed when the command getting run, couldn't find command. Fixed that

--- install_command.sh
+++ install_command.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Set the path to the IntelliJ IDEA bin directory
+IDEA_BIN="$(pwd)/bin"
 
-# Set the path to the idea.sh script
-IDEA_SH="$(pwd)/idea.sh"
+# Create the ~/.ide_java directory if it doesn't exist
+mkdir -p "/home/$USER/.ide_java"
+
+# Move the entire IntelliJ IDEA directory to the hidden ide_java directory in the user's home directory
+mv "$(dirname "$(pwd)")" "/home/$USER/.ide_java/ideaIC-2022.3.2"
+
+# Set the path to the idea.sh script in the new directory location
+IDEA_SH="/home/$USER/.ide_java/ideaIC-2022.3.2/bin/idea.sh"
 
 # Create the alias to open the current directory in IntelliJ IDEA
 alias ideadir="$IDEA_SH ."
-
-# Create the alias to open a file or directory in IntelliJ IDEA
-alias openjava="ideafile > /dev/null 2>&1 &"
 
 # Define the function to open a file or directory in IntelliJ IDEA
 function ideafile() {
@@ -21,18 +26,21 @@ function ideafile() {
 
     # Check if it's a directory or a file
     if [ -d "$file_path" ]; then
-        "$IDEA_SH" "$file_path"
+        nohup "$IDEA_SH" "$file_path" >/dev/null 2>&1 &
     elif [ -f "$file_path" ]; then
         # Open the file in IntelliJ IDEA
-        "$IDEA_SH" "$file_path"
+        nohup "$IDEA_SH" "$file_path" >/dev/null 2>&1 &
     else
         echo "File or directory not found: $file_path"
     fi
 }
 
+# Create the alias to open a file or directory in IntelliJ IDEA
+alias openjava="ideafile"
+
 # Save the aliases and function to the .bashrc file
 echo "alias ideadir='$IDEA_SH .'" >> ~/.bashrc
-echo "alias openjava='ideafile > /dev/null 2>&1 &'" >> ~/.bashrc
+echo "alias openjava='ideafile'" >> ~/.bashrc
 echo "function ideafile() {
     if [ \$# -eq 0 ]; then
         file_path=\$(realpath \".\")
@@ -41,9 +49,9 @@ echo "function ideafile() {
     fi
 
     if [ -d \"\$file_path\" ]; then
-        $IDEA_SH \"\$file_path\"
+        nohup $IDEA_SH \"\$file_path\" >/dev/null 2>&1 &
     elif [ -f \"\$file_path\" ]; then
-        $IDEA_SH \"\$file_path\"
+        nohup $IDEA_SH \"\$file_path\" >/dev/null 2>&1 &
     else
         echo \"File or directory not found: \$file_path\"
     fi
